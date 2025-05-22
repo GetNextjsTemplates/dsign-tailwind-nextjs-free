2ceaac00b9610f811e2957bf3e3df2f068d4d0c1
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-05-22 18:20:44 +0530

    "

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
-
+import ScrollToTop from './components/ScrollToTop';
 
 export const metadata = {
   title: 'dSign',
@@ -19,6 +19,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <ScrollToTop />
       </body>
     </html>
   )
